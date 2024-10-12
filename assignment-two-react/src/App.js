b69822407be953e7d1328dd6189b0fee0d3111be
Abhirami007 <psabhirami015@gmail.com>
2024-10-12 19:28:51 -0400

    adding routes to access pages

--- assignment-two-react/src/App.js
+++ assignment-two-react/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Register from "./auth/Register";
+import Dashboard from "./components/Dashboard";
+import Login from "./auth/Login";
+import UserProfile from "./components/UserProfile";
+import UserList from "./components/UserList";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/profile" element={<UserProfile />} />
+        <Route path="/users" element={<UserList />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
