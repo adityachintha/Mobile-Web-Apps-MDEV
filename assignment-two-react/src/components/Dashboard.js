c708ef78c91386c332992c9b720267b0824a316d
Abhirami007 <psabhirami015@gmail.com>
2024-10-12 19:27:48 -0400

    created basic dashboard component

---
+++ assignment-two-react/src/components/Dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h2>Dashboard</h2>
+            <nav>
+                <Link to="/profile">Profile</Link>
+                <Link to="/users">User List</Link>
+            </nav>
+        </div>
+    );
+};
+
+export default Dashboard;
