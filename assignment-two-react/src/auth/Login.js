94bb15db94b59ea03bbd1604177f86f3003b1a26
Abhirami007 <psabhirami015@gmail.com>
2024-10-12 19:27:32 -0400

    added user login component

---
+++ assignment-two-react/src/auth/Login.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { auth } from "../firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { Link, useNavigate } from "react-router-dom";
+
+import "./Register.css";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/dashboard");
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div className="register-container">
+      <h2>Login</h2>
+      {error && <p className="error-message">{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          required
+          className="input-field"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          required
+          className="input-field"
+        />
+        <button type="submit" className="submit-button">
+          Login
+        </button>
+      </form>
+      <div>
+        Not yest registered? <Link to="/">Register</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
