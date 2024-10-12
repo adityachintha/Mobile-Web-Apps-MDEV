953fba65352b63f516d1dd1481e461c33c05152d
Aditya Chintha <adityachintha@gmail.com>
2024-10-12 05:23:35 -0400

    create .env file for firebase

--- assignment-two-react/src/firebase.js
+++ assignment-two-react/src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YAIzaSyDAz9Ni4l6RABqhOrEPqcIAyJqA2WO9Vm0",
-  authDomain: "web-apps-6adb3.firebaseapp.com",
-  projectId: "web-apps-6adb3",
-  storageBucket: "web-apps-6adb3.appspot.com",
-  messagingSenderId: "592723119087",
-  appId: "1:592723119087:web:e888504e2716eac0b0841a",
-  measurementId: "G-GHLPYMX6WL",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
