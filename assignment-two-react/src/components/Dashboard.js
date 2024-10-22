3dff56257d8fb38ec3cf61175643662f6b0deaa4
Aditya Chintha <adityachintha@gmail.com>
2024-10-22 05:21:07 -0400

    update dashboard with styling and containers for Three Widgets

--- assignment-two-react/src/components/Dashboard.js
+++ assignment-two-react/src/components/Dashboard.js
@@ -1,15 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "";
+import "./Dashboard.css";
 
 const Dashboard = () => {
   return (
-    <div>
-      <h2>Dashboard</h2>
-      <nav>
-        <Link to="/profile">Profile</Link>
-        <Link to="/users">User List</Link>
-      </nav>
+    <div className="dashboard-container">
+      <div className="banner">
+        <div className="banner-left">
+          <img
+            className="avatar"
+            alt="avatar"
+            src="https://xsgames.co/randomusers/avatar.php?g=pixel"
+          ></img>
+          <div className="bannertext">
+            <h2 className="title">Hello, Sam</h2>
+            <h2 className="subtitle">Welcome to your Dashboard!</h2>
+          </div>
+        </div>
+
+        <nav className="navigation">
+          <Link to="/profile" className="nav-link">
+            Profile
+          </Link>
+          <Link to="/users" className="nav-link">
+            User List
+          </Link>
+        </nav>
+      </div>
+
+      {/* Widgets Section */}
+      <div className="widgets-container">
+        <div className="widget" id="widget-1">
+          Widget 1
+        </div>
+        <div className="widget" id="widget-2">
+          Widget 2
+        </div>
+        <div className="widget" id="widget-3">
+          Widget 3
+        </div>
+      </div>
     </div>
   );
 };
