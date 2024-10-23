485c022e3862be3e9de2f769d3621d4ce96f7780
Abhirami007 <psabhirami015@gmail.com>
2024-10-23 18:06:44 -0400

    updating user profile page

--- assignment-two-react/src/components/UserProfile.js
+++ assignment-two-react/src/components/UserProfile.js
@@ -1,13 +1,56 @@
-// src/components/UserProfile.js
-import React from "react";
-import { auth } from "../firebase";
+import React, { useState, useEffect } from 'react';
+import { db } from '../firebase'; 
+import { collection, query, where, getDocs } from 'firebase/firestore';
+import "../css/Dashboard.css";
 
 const UserProfile = () => {
-  const user = auth.currentUser;
+  const [user, setUser] = useState(null);
+  const [userData, setUserData] = useState(null); 
+  const [loading, setLoading] = useState(true); 
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      const user = JSON.parse(storedUser);
+      setUser(user);
+      
+      // Fetch user data from Firestore based on email
+      const fetchUserData = async () => {
+        try {
+          const userQuery = query(
+            collection(db, "users"),
+            where("email", "==", user.email)
+          );
+          const querySnapshot = await getDocs(userQuery);
+          if (!querySnapshot.empty) {
+            const userDataDoc = querySnapshot.docs[0].data();
+            setUserData(userDataDoc);
+          } else {
+            console.log("No user data found in Firestore.");
+          }
+        } catch (error) {
+          console.error("Error fetching user data:", error);
+        } finally {
+          setLoading(false);
+        }
+      };
+
+      fetchUserData();
+    } else {
+      setLoading(false);
+    }
+  }, []); 
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
-    <div>
-      {user ? <p>Email: {user.email}</p> : <p>No user is logged in.</p>}
+    <div className="user-profile-widget">
+      <img src={userData ? "https://xsgames.co/randomusers/avatar.php?g=male" : "default-avatar-url"} // Optional: set a default avatar URL
+             alt="Profile" className="avatar" />
+      <h2>{userData.firstName + " " + userData.lastName}</h2>
+      <p>Email: {user.email}</p>
     </div>
   );
 };
