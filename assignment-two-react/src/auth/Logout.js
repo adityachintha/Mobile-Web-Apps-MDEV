6c7f8da826ffca325ebbad2dc93e755eb78ed718
Aditya Chintha <adityachintha@gmail.com>
2024-10-24 19:15:45 -0400

    Updated comment header and comments on logout js

--- assignment-two-react/src/auth/Logout.js
+++ assignment-two-react/src/auth/Logout.js
@@ -1,6 +1,11 @@
-import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+// Team Members - Aditya Chintha (200595829), Abhirami Pradeep Susi (200589663)
+// File  - Lagout.js
+// Mobile Web Apps Assignment 2
 
+import { useEffect } from "react"; //Importing use effect
+import { useNavigate } from "react-router-dom"; // importing navigate
+
+//Function for Logout
 const Logout = () => {
   const navigate = useNavigate();
 
