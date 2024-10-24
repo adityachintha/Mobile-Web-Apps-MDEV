894f49afac263aa621353a71a36d4427c5581ca0
Aditya Chintha <adityachintha@gmail.com>
2024-10-24 19:30:37 -0400

    updated comment header and comments for space station data.

--- assignment-two-react/src/components/SpaceStationData.js
+++ assignment-two-react/src/components/SpaceStationData.js
@@ -1,24 +1,29 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import "../css/Dashboard.css";
+// Team Members - Aditya Chintha (200595829), Abhirami Pradeep Susi (200589663)
+// File  - SpaceStationData.js
+// Mobile Web Apps Assignment 2
 
-const SpaceStationData = ({showLimited}) => {
-  const [data, setData] = useState(null);
-  const [loadingISS, setLoadingISS] = useState(true);
-  const [people, setPeople] = useState([]);
-  const [loadingPeople, setLoadingPeople] = useState(true);
+import React, { useState, useEffect } from "react"; //Importing usestate and useeffect
+import axios from "axios"; // Importing axios
+import "../css/Dashboard.css"; //Importing Dashboard
 
-  // Fetch ISS Position Data
+//Function for Space station data
+const SpaceStationData = ({ showLimited }) => {
+  const [data, setData] = useState(null); //usestate for lat and longitude position
+  const [loadingISS, setLoadingISS] = useState(true); // usestate for ISS
+  const [people, setPeople] = useState([]); // use state for people in space
+  const [loadingPeople, setLoadingPeople] = useState(true); // use state for for people in space
+
+  // Function to Fetch ISS Position Data
   const fetchSpaceData = async () => {
     const options = {
       method: "GET",
-      url: "http://api.open-notify.org/iss-now.json",
+      url: "http://api.open-notify.org/iss-now.json", // Open Notify API
     };
 
     try {
       const response = await axios.request(options);
       console.log(response.data);
-      setData(response.data);
+      setData(response.data); //set for all data
     } catch (error) {
       console.error("Error fetching ISS position data:", error);
     } finally {
@@ -26,17 +31,17 @@ const SpaceStationData = ({showLimited}) => {
     }
   };
 
-  // Fetch People in Space Data
+  // Function to Fetch People in Space Data
   const fetchPeopleData = async () => {
     const options = {
       method: "GET",
-      url: "http://api.open-notify.org/astros.json",
+      url: "http://api.open-notify.org/astros.json", // Open Notify API
     };
 
     try {
       const response = await axios.request(options);
       console.log(response.data);
-      setPeople(response.data.people); // Accessing the correct response structure
+      setPeople(response.data.people); // set for people
     } catch (error) {
       console.error("Error fetching people in space data:", error);
     } finally {
@@ -44,14 +49,14 @@ const SpaceStationData = ({showLimited}) => {
     }
   };
 
-  // Fetch data on component mount
+  // Use Effect
   useEffect(() => {
-    fetchSpaceData();
-    fetchPeopleData();
+    fetchSpaceData(); //Calling the function for space data
+    fetchPeopleData(); // calling the function for people data
   }, []);
 
   return (
-    <div className={!showLimited&&"space-data-container"}>
+    <div className={!showLimited && "space-data-container"}>
       <h1>NASA Space</h1>
 
       {/* ISS Position */}
@@ -61,8 +66,10 @@ const SpaceStationData = ({showLimited}) => {
         <div>
           <h3>ISS Position</h3>
           <div className="widgetcardSpace">
-            <p>Latitude: {data.iss_position.latitude} N</p>
-            <p>Longitude: {data.iss_position.longitude} E</p>
+            <p>Latitude: {data.iss_position.latitude} N</p>{" "}
+            {/* Latitude position */}
+            <p>Longitude: {data.iss_position.longitude} E</p>{" "}
+            {/* Longitude  position */}
           </div>
         </div>
       ) : (
@@ -76,15 +83,15 @@ const SpaceStationData = ({showLimited}) => {
         <div>
           <h3>People in Space Right Now</h3>
           <div className="widgetcardSpace">
-            <h1>{people.length}</h1>
+            <h1>{people.length}</h1> {/* Number of people*/}
           </div>
           <ul>
             {people.slice(0, 10).map((person, index) => (
               <li key={index}>
                 <p>
-                  <b>{person.name}</b>
+                  <b>{person.name}</b> {/* Person name */}
                 </p>
-                <p>Craft: {person.craft}</p>
+                <p>Craft: {person.craft}</p> {/* person Craft */}
               </li>
             ))}
           </ul>
