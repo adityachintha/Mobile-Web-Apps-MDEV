94b794726f37efc4d5230de288246b35244498a1
Abhirami007 <psabhirami015@gmail.com>
2024-10-24 20:00:15 -0400

    responsiveness updates

--- assignment-two-react/src/components/WeatherData.js
+++ assignment-two-react/src/components/WeatherData.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "../css/Dashboard.css";
+import "../css/WeatherData.css";
 
 const WeatherData = ({ showLimited }) => {
   const [weather, setWeather] = useState(null);
